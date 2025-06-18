55ac2bc90e9c8fcc8fca1bf0716fd43819cdc9e6
Hari Shankaran <harish@infinite.com>
2025-06-18 21:03:41 +0530

    Training

--- C#/Assignments/Assignment 1/Code1/Code1/Program.cs
+++ C#/Assignments/Assignment 1/Code1/Code1/Program.cs
@@ -6,7 +6,7 @@ namespace Code1
     class Program
     {
         static int num1, num2;
-
+        //-----------------------------------------Number is Equal or Not---------------------------------------
         static void Equal_Or_Not() //Checks whether the given 2 integers are equal or not
         {
             Console.Write("Enter the First integer: ");
@@ -26,11 +26,11 @@ namespace Code1
 
             Console.ReadLine();
         }
-
+        //------------------------------------------Number is Positive or Negative-------------------------------------------
         static void Pos_or_Neg() // Checks whether the given number is positive or negative
         {
             Console.WriteLine("Enter a Number: ");
-            int num1 = int.Parse(Console.ReadLine());
+            num1 = int.Parse(Console.ReadLine());
 
             if (num1 < 0)
             {
@@ -46,7 +46,7 @@ namespace Code1
             }
             Console.ReadLine();
         }
-
+        //-----------------------------------------Arithmetic Operation--------------------------------------
         static void Arithmetic_Operation() //Does the arithmetic operation of the given 2 numbers
         {
 
@@ -88,12 +88,49 @@ namespace Code1
                 Console.ReadLine(); 
             }
 
+
+        //-----------------------------------------Multiplication table of a number--------------------------------------
+        static void Mul_Tab()
+        {
+            Console.WriteLine("Enter the number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<=10; i++)
+            {
+                Console.WriteLine($"{num2} * {i} = {num2 * i}");
+            }
+            Console.ReadLine();
+        }
+
+        //-----------------------------------------Multiplication table of a number--------------------------------------
+            static void Triple_sum()
+        {
+            Console.WriteLine("Enter the first number: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            int sum = num1 + num2;
+
+            if (num1 == num2)
+            {
+                Console.WriteLine($"The Sum is: {sum}");
+                Console.WriteLine($"The triple of the sum is {sum * 3}");
+            }
+            Console.ReadLine();
+
+        }
+
+
         static void Main(string[] args)
         {
-            Program.Equal_Or_Not();
-            Program.Pos_or_Neg();
-            Program.Arithmetic_Operation();
+            //Program.Equal_Or_Not();
+            //Program.Pos_or_Neg();
+            //Program.Arithmetic_Operation();
+            //Program.Mul_Tab();
+            Program.Triple_sum();
         }
+
     }
 
       

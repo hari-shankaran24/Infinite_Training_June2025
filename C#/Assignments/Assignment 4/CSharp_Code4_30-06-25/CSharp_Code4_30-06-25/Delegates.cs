ee3db451319d7ebc6dfd18a0ca77af11aebda286
Hari Shankaran <harish@infinite.com>
2025-06-30 21:20:32 +0530

    Training

---
+++ C#/Assignments/Assignment 4/CSharp_Code4_30-06-25/CSharp_Code4_30-06-25/Delegates.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    public delegate void RingEventHandler(string message);
+    internal class MobilePhone
+    {
+
+        public event RingEventHandler OnRing;
+        public void ReceiveCall()
+        {
+            if (OnRing != null)
+            {
+                OnRing("Incoming Call");
+            }
+        }
+    }
+
+    class Subscriber
+    {
+        public void PlayRingtone(string message)
+        {
+            Console.WriteLine("Playing Ringtone" + message);
+        }
+
+        public void CallerInfo(string message)
+        {
+            Console.WriteLine("Displaying caller information" + message);
+        }
+        public void Vibrate(string message)
+        {
+            Console.WriteLine("Phone is vibrating" + message);
+        }
+
+
+    }
+    class EventDemo1
+    {
+        static void Main()
+        {
+            MobilePhone mob = new MobilePhone();
+            Subscriber subscriber = new Subscriber();
+
+
+            mob.OnRing += subscriber.PlayRingtone;
+            mob.OnRing += subscriber.CallerInfo;
+            mob.OnRing += subscriber.Vibrate;
+            mob.ReceiveCall();
+
+
+            Console.ReadLine();
+        }
+    }
+}
